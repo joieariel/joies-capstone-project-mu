a0b26120815855202e23515c916d9395768eedcb
Joie Ariel Whitmon <joiewhitmon@meta.com>
2025-06-25 12:21:52 -0700

    Add community center routes

---
+++ backend/routes/communityCenters.js
@@ -0,0 +1,23 @@
+const express = require("express"); //import express
+const { PrismaClient } = require("@prisma/client"); // require prisma client
+const router = express.Router(); // import express router
+const prisma = new PrismaClient(); // create new prisma client
+
+// (GET) get all community centers from database
+router.get("/", async (req, res) => {
+  const communityCenters = await prisma.CommunityCenter.findMany();
+  console.log(communityCenters);
+  res.json(communityCenters);
+});
+
+// (GET) get a specific community center from db
+router.get("/:communityCenterId", async (req, res) => {
+  const communityCenterId = parseInt(req.params.boardId);
+  const communityCenters = await prisma.CommunityCenter.findUnique({
+    where: { id: parseInt(communityCenterId) },
+  });
+  console.log(communityCenters);
+  res.json(communityCenters);
+});
+
+module.exports = router;
