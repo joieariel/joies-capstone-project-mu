da61e34dad5128d5f0129e8ff8e4b5db50a7a3a1
Joie Ariel Whitmon <joiewhitmon@meta.com>
2025-07-01 11:10:41 -0700

    Updated routing for mapview vs specific center on map

---
+++ frontend/src/SpecificMap.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SpecificMap.css";
+
+const SpecificMap = () => {
+  return (
+    <div className="map-container">
+      <div className="map-content">
+        <h1 className="map-title">Specific Map</h1>
+        <p className="map-description">
+          This is a placeholder for the map. Center on map from API will be displayed here!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SpecificMap;
